5e4f55e3c8e4a0f57baf4bb6f53e6f7a4e6c406a
aryan-sharma-1908 <aryansharma81828@gmail.com>
2025-11-17 21:49:55 +0530

    cors options added

--- server/app.js
+++ server/app.js
@@ -6,10 +6,12 @@ const morgan = require('morgan');
 const PORT = process.env.PORT || 5000;
 const authRoutes = require('./routes/auth.route.js');
 const connectDB = require('./database/database.js');
+const corsOptions = require('./cors/corsoptions.js');
+
 require('dotenv/config');
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(morgan('tiny'));
